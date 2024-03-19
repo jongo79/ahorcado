0dad85d1cad7b2329b2ae7998892d8f45d16994a
jongo79 <jongfad@gmail.com>
2024-03-19 13:51:07 +0100

    Keyboard

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 
 const HangmanGame = () => {
@@ -28,7 +28,10 @@ const HangmanGame = () => {
     const generateHiddenPhrase = (phrase) => {
         const { mainPhrase } = splitPhrase(phrase);
         const normalizedPhrase = _.deburr(mainPhrase);
-        return normalizedPhrase.replace(/\S/g, '_').replace(/ /g, '|');
+        return normalizedPhrase
+            .replace(/\S/g, '_') // Replace letters with underscores
+            .replace(/ /g, '|') // Replace spaces with pipes
+            .replace(/,/g, ', '); // Replace commas with commas followed by a space
     };
 
     const splitPhrase = (phrase) => {
@@ -79,22 +82,94 @@ const HangmanGame = () => {
         setLastLetter(letter);
     };
 
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            const letter = event.key.toUpperCase();
+            if (/^[A-Z]$/.test(letter)) {
+                handleGuess(letter);
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleGuess]);
+    
+
     return (
         <div>
             <h1>Hangman Game</h1>
-            <button onClick={handleStartGame}>Start Game</button>
-            <p>Phrase: {hiddenPhrase} - {splitPhrase(phrase).author}</p>
+            <button onClick={handleStartGame} autoFocus>Start Game</button>
+            <p>Phrase: {hiddenPhrase.split('').map((char, index) => {
+                if (char === '|') {
+                    return ' ';
+                } else {
+                    return char;
+                }
+            }).join('')} - {splitPhrase(phrase).author}</p>
             <p>Lives: {lives}</p>
             <p>Last Letter: {lastLetter}</p>
             {gameOver && <p>Game Over!</p>}
             {gameWon && <p>Premio!</p>}
             <div>
-                {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => (
-                    <button key={letter} onClick={() => handleGuess(letter)} disabled={usedLetters.has(letter.toLowerCase())}>
-                        {letter}
-                    </button>
-                ))}
-            </div>
+
+        <div>
+
+        <div className="charKeyboard">
+       
+        <button className="button" onClick={() => handleGuess('Q')}>Q</button>
+        <button className="button" onClick={() => handleGuess('E')}>E</button>
+        <button className="button" onClick={() => handleGuess('R')}>R</button>
+        <button className="button" onClick={() => handleGuess('T')}>T</button>
+        <button className="button" onClick={() => handleGuess('Y')}>Y</button>
+        <button className="button" onClick={() => handleGuess('U')}>U</button>
+        <button className="button" onClick={() => handleGuess('I')}>I</button>
+        <button className="button" onClick={() => handleGuess('O')}>O</button>
+        <button className="button" onClick={() => handleGuess('P')}>P</button>
+        <br />
+        <button className="button" onClick={() => handleGuess('A')}>A</button>
+        <button className="button" onClick={() => handleGuess('S')}>S</button>
+        <button className="button" onClick={() => handleGuess('D')}>D</button>
+        <button className="button" onClick={() => handleGuess('F')}>F</button>
+        <button className="button" onClick={() => handleGuess('G')}>G</button>
+        <button className="button" onClick={() => handleGuess('H')}>H</button>
+        <button className="button" onClick={() => handleGuess('J')}>J</button>
+        <button className="button" onClick={() => handleGuess('K')}>K</button>
+        <button className="button" onClick={() => handleGuess('L')}>L</button>
+        <button className="button" onClick={() => handleGuess('Ñ')}>Ñ</button>
+        <br />
+        <button className="button" onClick={() => handleGuess('Z')}>A</button>
+        <button className="button" onClick={() => handleGuess('X')}>S</button>
+        <button className="button" onClick={() => handleGuess('C')}>D</button>
+        <button className="button" onClick={() => handleGuess('V')}>F</button>
+        <button className="button" onClick={() => handleGuess('B')}>G</button>
+        <button className="button" onClick={() => handleGuess('N')}>H</button>
+        <button className="button" onClick={() => handleGuess('M')}>J</button>
+        <br />
+        </div>
+        <div className="numKeyboard">
+
+        <button className="button" onClick={() => handleGuess('1')}>1</button>
+        <button className="button" onClick={() => handleGuess('2')}>2</button>
+        <button className="button" onClick={() => handleGuess('3')}>3</button>       
+        <br />
+        <button className="button" onClick={() => handleGuess('4')}>4</button>
+        <button className="button" onClick={() => handleGuess('5')}>5</button>
+        <button className="button" onClick={() => handleGuess('6')}>6</button>       
+        <br />
+        <button className="button" onClick={() => handleGuess('7')}>7</button>
+        <button className="button" onClick={() => handleGuess('8')}>8</button>
+        <button className="button" onClick={() => handleGuess('9')}>9</button>       
+        <br />
+        <button className="button" onClick={() => handleGuess('#')}>4</button>
+        <button className="button" onClick={() => handleGuess('0')}>5</button>
+        <button className="button" onClick={() => handleGuess('*')}>6</button>       
+        <br />
+        </div>       
+      </div>
+    </div>
         </div>
     );
 };
